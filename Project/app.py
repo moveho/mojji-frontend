1ed00b9f5542058256c46c4a58bba5c88b14d927
Ubuntu <ubuntu@ip-172-31-59-213.ap-northeast-2.compute.internal>
2023-07-24 12:15:18 +0900

    add requirements.txt file

--- Project/app.py
+++ Project/app.py
@@ -959,5 +959,5 @@ def oauth_userinfo_api():
     result = Oauth().userinfo("Bearer " + access_token)
     return jsonify(result)
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
